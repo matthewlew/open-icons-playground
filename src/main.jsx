d13cd14ef0f7afc816c26d3c501bcf5ff192e7a8
Matthew Lew <20917723+matthewlew@users.noreply.github.com>
2026-03-06 01:55:49 -0500

    Add routing with BrowserRouter and ReadmePage

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from '../open-icons-playground_12_3.jsx'
+import ReadmePage from './ReadmePage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/docs" element={<ReadmePage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
